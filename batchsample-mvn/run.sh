af1ee11effe2546c7eb77791e7a6db8e2cfec52f
Satoshi OKANO <satokano@gmail.com>
2013-12-23 03:31:01 +0900

    shell

--- batchsample-mvn/run.sh
+++ batchsample-mvn/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-java -cp ./lib/*:./config/ org.springframework.batch.core.launch.support.CommandLineJobRunner classpath:/launch-context.xml job1 input.file.name=./pom.xml output.file.name=./hoge.xml
+java -cp ./lib/*:./config/ org.springframework.batch.core.launch.support.CommandLineJobRunner classpath:/launch-context.xml job1 input.file.name=file:./pom.xml output.file.name=file:./out.data -next
 
